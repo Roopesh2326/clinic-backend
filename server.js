1c4f26f7c4b1373ad486f42c57d419b894d76c8d
unknown <roopeshdeep52@gmail.com>
2026-03-30 23:06:03 +0530

    added notice api

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
+const Notice = require("./models/Notice");
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const fs = require("fs");
+//connect database 
+mongoose.connect("mongodb+srv://roopeshdeep:<db_password>@cluster0.00b27mo.mongodb.net/?appName=Cluster0")
+.then(() => console.log("MongoDB connected"))
+.catch((error) => console.log(error));
 
 const filePath = "appointments.json";
 
@@ -33,4 +40,28 @@ app.get("/appointments", (req, res) => {
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
+});
+
+
+// ✅ GET NOTICE
+app.get("/notice", async (req, res) => {
+  const notice = await Notice.findOne();
+  res.json(notice);
+});
+
+// ✅ UPDATE NOTICE
+app.post("/notice", async (req, res) => {
+  const { message } = req.body;
+
+  let notice = await Notice.findOne();
+
+  if (notice) {
+    notice.message = message;
+    await notice.save();
+  } else {
+    notice = new Notice({ message });
+    await notice.save();
+  }
+
+  res.json({ message: "Notice updated" });
 });
